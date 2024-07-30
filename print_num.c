46edf0a3516cea32056f3dd52f3fe02eead26e70
W-Hughes <willskhughes.edu@gmail.com>
2024-07-30 14:55:51 -0700

    Create print_num.c file

---
+++ print_num.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *print_num - Prints an int argument.
+ *@list: argument list.
+ *Return: Length of characters printed.
+ */
+
+int print_num(va_list list)
+{
+	int o_num;
+	int p_ten;
+	int len;
+	unsigned int abs_num;
+
+	o_num = va_arg(list, int);
+	p_ten = 1;
+	len = 0;
+
+	if (o_num < 0)
+	{
+		len += _putchar('-');
+		abs_num = -o_num;
+	}
+
+	else
+		abs_n = o_num;
+
+	while (abs_num / p_ten > 9)
+	{
+		p_ten *= 10;
+	}
+	while (p_ten != 0)
+	{
+		len += _putchar('0' + abs_num / p_ten);
+		abs_num %= p_ten;
+		p_ten /= 10;
+	}
+
+	return (len);
+
+}
